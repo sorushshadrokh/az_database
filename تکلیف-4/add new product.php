b48fe65b16cd70ae17e195e98a4cb3c12e1f872b
sorushshadrokh <127998389+sorushshadrokh@users.noreply.github.com>
2023-11-11 11:48:08 +0330

    اضافه کردن محصول

---
+++ تکلیف-4/add new product.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test1";
+
+// 1.create connection
+$conn = mysqli_connect($servername, $username , $password , $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+//2.query
+$sql = "INSERT INTO `product`(`name`, `brand`, `Purchase_price`, `Quantity_in_stock`, `Category`, `price`)
+ VALUES ('یخچال اسنووا مدل 3551','snowa',40000000,20,'یخچال',42000000)";
+$result = mysqli_query($conn , $sql);
+
+//3.close conection
+mysqli_close($conn);
+
+?>
